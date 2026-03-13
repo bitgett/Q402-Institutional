19b6aa5928304c89961163213f8b7cc12d9f5d32
bitgett <bitgett@github.com>
2026-03-13 13:21:18 +0900

    feat: add team wallet to master whitelist

--- app/lib/access.ts
+++ app/lib/access.ts
@@ -2,6 +2,7 @@
 const MASTER_ADDRESSES = [
   "0xfc77ff29178b7286a8ba703d7a70895ca74ff466",
   "0xf5cdcd89b7dae1484197a4a65b97cd7a5e945c28",
+  "0x3717D6Ed5C2BCe558E715cDa158023dB6705fD47",
 ];
 
 export function isPaid(address: string): boolean {
